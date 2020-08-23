e98dd1eff4a8917f80e52f480dd4fae4440026d2
tadashi-aikawa <syou.maman@gmail.com>
2020-08-23 20:49:07 +0900

    🔨 Use logger instead of print

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 extern crate anyhow;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate log;
 
 use std::fs;
 use std::path::PathBuf;
 
 use anyhow::Result;
+use env_logger::Env;
 use serde::Deserialize;
 use structopt::StructOpt;
 
@@ -57,19 +60,24 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+
     let args: Args = Args::from_args();
     let json_str = fs::read_to_string(&args.input)?;
     let operation = serde_json::from_str::<Operation>(&json_str)?;
 
     if let Some(op) = operation.create_groups {
+        info!(">>>>>>>>>> Create groups");
         do_create_groups(&op).await
     }
 
     if let Some(op) = operation.invite_members {
+        info!(">>>>>>>>>> Invite members");
         do_invite_members(&op).await
     }
 
     if let Some(op) = operation.add_group_members {
+        info!(">>>>>>>>>> Add members to groups");
         do_add_group_members(&op).await
     }
 
@@ -79,10 +87,10 @@ async fn main() -> Result<()> {
 async fn do_create_groups(op: &CreateGroupsOperation) {
     for group_name in op.group_names.iter() {
         match bitbucket::v1api::post_groups(&op.workspace_uuid, &group_name).await {
-            Ok(group) => println!("Create a new group, {}!!", group.name),
+            Ok(group) => info!("Create a new group, {}!!", group.name),
             Err(err) => {
-                println!("Fail to create a new group, {}..", group_name);
-                println!("{}", err)
+                error!("Fail to create a new group, {}..", group_name);
+                error!("{}", err)
             }
         }
     }
@@ -91,10 +99,10 @@ async fn do_create_groups(op: &CreateGroupsOperation) {
 async fn do_invite_members(op: &InviteMembersOperation) {
     for email in op.emails.iter() {
         match bitbucket::v1api::post_invitations(&op.repository, &op.permission, &email).await {
-            Ok(_) => println!("Invite {}!!", &email),
+            Ok(_) => info!("Invite {}!!", &email),
             Err(err) => {
-                println!("Fail to invite {}..", &email);
-                println!("{}", err)
+                error!("Fail to invite {}..", &email);
+                error!("{}", err)
             }
         }
     }
@@ -104,10 +112,10 @@ async fn do_add_group_members(op: &AddGroupMembersOperation) {
     for group in op.groups.iter() {
         for email in group.emails.iter() {
             match bitbucket::v1api::put_group_member(&op.workspace_uuid, &group.slug, email).await {
-                Ok(_) => println!("Add {} to {}!!", &email, &group.slug),
+                Ok(_) => info!("Add {} to {}!!", &email, &group.slug),
                 Err(err) => {
-                    println!("Fail to add {} to {}..", &email, &group.slug);
-                    println!("{}", err)
+                    error!("Fail to add {} to {}..", &email, &group.slug);
+                    error!("{}", err)
                 }
             }
         }
